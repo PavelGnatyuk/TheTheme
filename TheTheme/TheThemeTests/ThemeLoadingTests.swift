5fb650ca00d6ac395c261fa9d8dfadcd98842545
PavelGnatyuk <pgnatyuk@gmail.com>
2019-03-30 19:58:51 +0300

    merge conflicts

---
+++ TheTheme/TheThemeTests/ThemeLoadingTests.swift
@@ -0,0 +1,47 @@
+//
+//  ThemeLoadingTests.swift
+//  TheThemeTests
+//
+//  Created by Pavel Gnatyuk on 04/03/2019.
+//  Copyright © 2019 Pavel Gnatyuk. All rights reserved.
+//
+
+import XCTest
+@testable import TheTheme
+
+class ThemeLoadingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
+
+fileprivate extension Data {
+    static let jsonTest = """
+[
+    {
+        "name": "Test Theme",
+        "description": "Theme for the unit testing."
+    },
+    {
+    }
+]
+""".data(using: .utf8)!
+}
